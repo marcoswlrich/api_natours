338c5fa82aeb8bed59059efc4052f8bdd5d7512b
marcoswlrich <marcosapw.dev@gmail.com>
2022-02-18 00:01:26 -0300

    Coreções e testes

--- server.ts
+++ server.ts
@@ -1,6 +1,24 @@
 import express from 'express';
+import * as fs from 'fs';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Olá');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
 
 const port = 4007;
 app.listen(port, () => console.log(`App running on port ${port}...`));
