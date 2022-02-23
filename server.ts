10d9d4f9cf5a5d0b101b250e9b64487e7f41f99a
marcoswlrich <marcosapw.dev@gmail.com>
2022-02-22 23:33:03 -0300

    GET & POST

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Olá');
 });
@@ -18,6 +20,44 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find(tour => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+  return res.status(201).json(tour);
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    sucess => {
+      res.status(201).json({
+        status: 'sucess',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 const port = 4007;
