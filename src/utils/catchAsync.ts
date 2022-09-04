0e0ea0c45bc8fc5ce7959dc919071ed103b28fbe
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-03 22:04:49 -0400

    catchAsync

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,29 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
-type CbRetPromise<P, ResBody, ReqBody, ReqQuery> = (
-  req: Request<P, ResBody, ReqBody, ReqQuery>,
-  res: Response,
-  next: NextFunction,
-) => Promise<void>;
-type returnedFn<P, ResBody, ReqBody, ReqQuery> = (
-  req: Request<P, ResBody, ReqBody, ReqQuery>,
-  res: Response,
-  next: NextFunction,
-) => void;
-interface IDictionary {
-  [key: string]: string;
-}
-type Query = Partial<IDictionary>;
-// Request<P = core.ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = qs.ParsedQs
-function catchAsync<
-  P = IDictionary,
-  ResBody = any,
-  ReqBody = any,
-  ReqQuery = Query,
->(
-  fn: CbRetPromise<P, ResBody, ReqBody, ReqQuery>,
-): returnedFn<P, ResBody, ReqBody, ReqQuery> {
-  return (req, res, next) => fn(req, res, next).catch(next);
-}
-
-export default catchAsync;
+export const catchAsync = (
+  fn: (req: Request, res: Response, next: NextFunction) => any,
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+};
