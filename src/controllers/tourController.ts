9ec0735969797257ab9985381da2afd48e895afc
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-04 00:46:54 -0400

    Correcoes TourController

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -12,6 +12,16 @@ import {
 // import { APIFeatures } from '../utils/APIFeatures';
 import { catchAsync } from '../utils/catchAsync';
 
+export const aliasTopTours = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
 // );
