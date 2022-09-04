d0c47b4a50742a2888329132f8e12d00343d381a
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-04 00:46:17 -0400

    Correcoes UserController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,3 +1,5 @@
+import { NextFunction, Request, Response } from 'express';
+
 import { UserModel } from '../models/userModel';
 import { deleteOne, getAll, getOne, updateOne } from '../services/UserService';
 
@@ -5,3 +7,10 @@ export const getUser = getOne(UserModel);
 export const getAllUsers = getAll(UserModel);
 export const updateUser = updateOne(UserModel);
 export const deleteUser = deleteOne(UserModel);
+
+export const createUser = (req: Request, res: Response) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use sign up instead',
+  });
+};
