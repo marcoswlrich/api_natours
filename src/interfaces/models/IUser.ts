eb4aed40737362561fa4f617b0a22962df724dd7
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-03 23:49:05 -0400

    Interface User

--- src/interfaces/models/IUser.ts
+++ src/interfaces/models/IUser.ts
@@ -1,15 +1,26 @@
-interface IUser {
+import mongoose from 'mongoose';
+
+export interface IUser {
   name: string;
   email: string;
-  password: string;
-  confirmPassword: string | undefined;
   photo?: string;
-  role: 'user' | 'guide' | 'lead-guide' | 'admin';
-  active: boolean;
+  password: string;
+  passwordConfirm: string | undefined;
+  passwordChangedAt?: Date;
+}
+
+export interface IUserDoc extends mongoose.Document {
+  name: string;
+  email: string;
+  photo?: string;
+  password: string;
+  role?: string;
+  passwordConfirm: string | undefined;
   passwordChangedAt?: Date;
   passwordResetToken?: string;
   passwordResetExpires?: Date;
+  active?: boolean;
+  correctPassword(password: string, hash: string): boolean;
+  changedPasswordAfter(JWTTimestamp: number): boolean;
   createPasswordResetToken(): string;
 }
-
-export { IUser };
