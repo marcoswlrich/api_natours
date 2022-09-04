eeb76b3e6563c29aa3bafee3ddec91015a94071f
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-03 23:52:22 -0400

    New Config

--- src/interfaces/express/IRequest.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express';
-
-import { IUserWithoutPassword } from '../dtos/IUserDto';
-
-interface IRequest extends Request {
-  user: IUserWithoutPassword;
-}
-
-export { IRequest };
