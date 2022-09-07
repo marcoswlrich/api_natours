1589c299283f5fdf91c8e561fc9b7f66d05c0a0e
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-07 15:25:24 -0400

    Auth rote user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,33 @@
 import express from 'express';
 
 import {
+  forgotPassword,
+  login,
+  protect,
+  resetPassword,
+  signup,
+  updatePassword,
+} from '../controllers/authController';
+import {
   createUser,
+  deleteMe,
   deleteUser,
   getAllUsers,
   getUser,
+  updateMe,
   updateUser,
 } from '../controllers/userController';
 
 const userRouter = express.Router();
+
+userRouter.post('/signup', signup);
+userRouter.post('/login', login);
+userRouter.post('/forgotPassword', forgotPassword);
+userRouter.patch('/resetPassword/:resetToken', resetPassword);
+
+userRouter.patch('/updatePassword', protect, updatePassword);
+userRouter.patch('/updateMe', updateMe);
+userRouter.delete('/deleteMe', deleteMe);
 
 userRouter.route('/').get(getAllUsers).post(createUser);
 
