aef729df256199ccdb499a06e7a66f84d0ee8b47
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-07 23:41:14 -0400

    Config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
+import mongoSanitize from 'express-mongo-sanitize';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import hpp from 'hpp';
 import morgan from 'morgan';
 
 import { globalErrorHandler } from './controllers/errorController';
@@ -8,11 +12,36 @@ import { userRouter } from './routes/userRoutes';
 
 const app = express();
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan<Request, Response>('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this Ip, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'price',
+      'ratingsAverage',
+      'difficulty',
+    ],
+  }),
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use(
